b3d2b4c8893d4f55f123b0cb3faae775fee5c397
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-08-22 14:28:29 -0400

    Use asciidoctor instead of asciidoc

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 cat ./stylesheets/reset.css ./stylesheets/cognitory.css \
   > /tmp/intro-to-programming.css
 
-asciidoc \
+asciidoctor \
   -a stylesheet=/tmp/intro-to-programming.css \
   -a theme='' index.asciidoc
